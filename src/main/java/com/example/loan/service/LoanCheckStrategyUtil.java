858a3543528c2fc3951fd23f89dbf4e6f9534692
mac <lishuang_read@126.com>
2021-11-07 22:12:27 +0800

    refactor it

--- src/main/java/com/example/loan/service/LoanCheckStrategyUtil.java
+++ src/main/java/com/example/loan/service/LoanCheckStrategyUtil.java
@@ -6,19 +6,11 @@ import com.example.loan.bo.Gender;
 public class LoanCheckStrategyUtil {
 
     public static CheckResultBo checkeLenderAndLoanTerm(Gender gender, Integer lenderAge, Integer loanTerm) {
-        CheckResultBo result =new CheckResultBo();
-        if(gender == Gender.MALE){
-            if (lenderAge + loanTerm <= 65) {
-                result = new CheckResultBo(true, "");
-            }else{
-                result = new CheckResultBo(false, gender.getDesc() + "_年龄_加_贷款年限_不能超过" + gender.getMaxAgePlusLoanTermValue());
-            }
-        }else if(gender == Gender.FEMALE) {
-            if (lenderAge + loanTerm <= 60) {
-                result = new CheckResultBo(true, "");
-            }else{
-                result = new CheckResultBo(false, gender.getDesc() + "_年龄_加_贷款年限_不能超过" + gender.getMaxAgePlusLoanTermValue());
-            }
+        CheckResultBo result = new CheckResultBo();
+        if (lenderAge + loanTerm <= gender.getMaxAgePlusLoanTermValue()) {
+            result = new CheckResultBo(true, "");
+        } else {
+            result = new CheckResultBo(false, gender.getDesc() + "_年龄_加_贷款年限_不能超过" + gender.getMaxAgePlusLoanTermValue());
         }
         return result;
     }
