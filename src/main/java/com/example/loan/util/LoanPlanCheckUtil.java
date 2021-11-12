0b582b106f08cc6f2dc64243cf00a561691c4b46
lis05 <lishuang_read@126.com>
2021-11-12 22:36:23 +0800

    住房贷款年限最长为30年 refactor it

--- src/main/java/com/example/loan/util/LoanPlanCheckUtil.java
+++ src/main/java/com/example/loan/util/LoanPlanCheckUtil.java
@@ -8,9 +8,12 @@ import com.example.loan.bo.UserLoanPlanMaterial;
  * @date ：Created in 2021/11/12 19:16
  */
 public class LoanPlanCheckUtil {
+
+    public static final int MAX_LOAN_TERM = 30;
+
     public static LoanPlantChectBo check(Integer loanTerm, UserLoanPlanMaterial loaner) {
-        if (loanTerm > 30) {
-            return new LoanPlantChectBo("住房贷款年限最长为30年",true);
+        if (loanTerm > MAX_LOAN_TERM) {
+            return new LoanPlantChectBo("住房贷款年限最长为" + MAX_LOAN_TERM + "年",true);
         }
         if (loaner.getLenderAge() + loanTerm > 65) {
             return new LoanPlantChectBo("男性_年龄_加_贷款年限_不能超过65",false);
