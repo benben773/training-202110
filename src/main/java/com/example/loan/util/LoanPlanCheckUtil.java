dd0338be52f0373a1b2d3604f39be0e72d5b86fa
lis05 <lishuang_read@126.com>
2021-11-12 22:39:27 +0800

    女性_年龄_加_贷款年限_不能超过60 pass it

--- src/main/java/com/example/loan/util/LoanPlanCheckUtil.java
+++ src/main/java/com/example/loan/util/LoanPlanCheckUtil.java
@@ -1,5 +1,6 @@
 package com.example.loan.util;
 
+import com.example.loan.bo.Gender;
 import com.example.loan.bo.LoanPlantChectBo;
 import com.example.loan.bo.UserLoanPlanMaterial;
 
@@ -15,8 +16,11 @@ public class LoanPlanCheckUtil {
         if (loanTerm > MAX_LOAN_TERM) {
             return new LoanPlantChectBo("住房贷款年限最长为" + MAX_LOAN_TERM + "年",true);
         }
-        if (loaner.getLenderAge() + loanTerm > 65) {
+        if (loaner.getGender() == Gender.MALE && loaner.getLenderAge() + loanTerm > 65) {
             return new LoanPlantChectBo("男性_年龄_加_贷款年限_不能超过65",false);
+        }
+        if (loaner.getGender() == Gender.FEMALE && loaner.getLenderAge() + loanTerm > 60) {
+            return new LoanPlantChectBo("女性_年龄_加_贷款年限_不能超过60",false);
         }
         return new LoanPlantChectBo("",true);
     }
