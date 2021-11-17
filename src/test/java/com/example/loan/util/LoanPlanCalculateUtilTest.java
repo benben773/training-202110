aa2f99fe97f1f4be346675b67cb49c1002eab207
lis05 <lishuang_read@126.com>
2021-11-17 20:54:46 +0800

    贷款年限加上房龄总、贷款年限不能超过60取最小 create a new test and pass it

--- src/test/java/com/example/loan/util/LoanPlanCalculateUtilTest.java
+++ src/test/java/com/example/loan/util/LoanPlanCalculateUtilTest.java
@@ -37,6 +37,7 @@ public class LoanPlanCalculateUtilTest {
         return Arrays.asList(new Object[][]{
                 {Gender.MALE, 35,30,0,true,30,"最大贷款年限计算_男性_年龄_加_贷款年限_不能超过65"},
                 {Gender.MALE, 35,20,20,true,20,"贷款年限加上房龄总和不能超过40年"},
+                {Gender.FEMALE, 35,26,20,true,20,"贷款年限加上房龄总、贷款年限不能超过60取最小"},
         });
     }
     @Test
